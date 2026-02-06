4fc198848109548e5afea8c4949bc08604a227e8
Anis-21 <anis.benabbou@gmail.com>
2026-02-06 11:15:08 +0100

    feature/channel: solid bases. Next: developping commands

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -10,22 +10,30 @@ class Channel {
     private :
         std::string _name;
         std::string _topic;
+        std::string _password;
         std::vector<Client *> _admin;
         std::vector<Client *> _users;
 
         Channel();
         
     public :
-        Channel(std::string name);
+        Channel(std::string name, std::string topic, std::string password);
         Channel(Channel const &src);
+        Channel &operator=(Channel const &rhs);
         ~Channel();
         
         void                addAdmin(Client *user);
         void                addClient(Client *user);
         void                removeClient(Client *user);
 
-        const std::string   &getChannelName() const;
-        const std::string   &getChannelTopic() const;
-        void                setChannelTopic(std::string topic);
+        std::string const &getChannelName() const;
+        std::string const &getChannelTopic() const;
+        std::string const &getPasswordChannel() const;
 
+        void                setChannelTopic(Client *user, std::string topic);
+
+        class NotAdmin : public std::exception {
+            public :
+                const char *what() const throw();
+        };
 };
