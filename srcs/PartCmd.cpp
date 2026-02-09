f568626482e456727e3858480e774718aa322e06
D2BT <quentindbbt@gmail.com>
2026-02-09 15:15:25 +0100

    avancee sur PartCmd

--- srcs/PartCmd.cpp
+++ srcs/PartCmd.cpp
@@ -10,10 +10,15 @@ void PartCmd::execute(Server& server, Client& client, const std::vector<std::str
 		return;
 	}
 	std::vector<std::string> channelsToLeave;
-	std::string channelToLeave = args[0];
-	if (channelToLeave.find(',')){
-		
+	if (args[0].find(',')){
+		for(size_t i = 0; i < args[0].size(); i++){
+			int j = 0;
+			while (args[0][j] && args[0][j] != ,)
+		}
+		for(size_t i = 0; i < args[0].size(); i++){	channelsToLeave[i].substr(1);}
 	}
 	else
-		channelsToLeave.push_back(channelToLeave.substr(1));
+		channelsToLeave.push_back(args[0].substr(1));
+
+
 }
