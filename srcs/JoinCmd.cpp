c78c675da27154d1e3de14e39d9768b6f896deea
Anis-21 <anis.benabbou@gmail.com>
2026-02-09 17:39:31 +0100

    Key handled for Channels

--- srcs/JoinCmd.cpp
+++ srcs/JoinCmd.cpp
@@ -21,28 +21,35 @@ void JoinCmd::execute(Server &server, Client &client, std::vector<std::string> c
     if (args.size() > 1)
         keys = split(args[1], ',');
     for (size_t i = 0; i < channelNames.size(); i++){
-        std::string key = (i < channelNames.size() ? keys[i] : "");
+        std::string key = (i < keys.size() ? keys[i] : "");
         joinChannel(server, client, channelNames[i], key);
     }
     
 }
 
 void JoinCmd::joinChannel(Server &server, Client &client, std::string channelName, std::string key){
-    (void)key;
     if (channelName[0] != '#')
         return;
     Channel *channel = server.getChannel(channelName);
     if (channel == NULL){
-        channel = server.createChannel(channelName.substr(1, channelName.size() - 1));
+        channel = server.createChannel(channelName, key);
         channel->addAdmin(&client);
+    }
+    else {
+        std::string channelPwd = channel->getPasswordChannel();
+        if (!channelPwd.empty() && key != channelPwd){
+            server.sendReply(client, ":" + server.getServerName() + " 475 " + client.getNickname() + " " + channelName + " : Cannot join channel (+k)\r\n");
+            return;
+        }
+
     }
     channel->addClient(&client);
     std::string joinMsg = ":" + client.getNickname() + "!" + client.getUsername() + "@localhost JOIN :" + channelName + "\r\n";
     channel->broadcastMessage(server, joinMsg);
     if (channel->getChannelTopic().empty())
-        server.sendReply(client, ":" + server.getServerName() + " 331 " + client.getNickname() + " #" + channelName + ": No Topic is set" + "\r\n");
+        server.sendReply(client, ":" + server.getServerName() + " 331 " + client.getNickname() + " " + channelName + ": No Topic is set" + "\r\n");
     else
-        server.sendReply(client, ":" + server.getServerName() + " 332 " + client.getNickname() + " #" + channelName + ": Topic is " + channel->getChannelTopic() + "\r\n");
-    server.sendReply(client, ":" + server.getServerName() + " 353 " + client.getNickname() + "#" + channelName + " :" + channel->getUserList());
-    server.sendReply(client, ":" + server.getServerName() + " 366" + client.getNickname() + " #" + channelName + " :End of /NAMES list");
+        server.sendReply(client, ":" + server.getServerName() + " 332 " + client.getNickname() + " " + channelName + ": Topic is " + channel->getChannelTopic() + "\r\n");
+    server.sendReply(client, ":" + server.getServerName() + " 353 " + client.getNickname() + " = " + channelName + " :" + channel->getUserList());
+    server.sendReply(client, ":" + server.getServerName() + " 366" + client.getNickname() + " " + channelName + " :End of /NAMES list");
 }
